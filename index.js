73be1662b098dc3573096b72fa7d9f25220f5969
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-09 15:50:00 +0900

    complete the challenge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Change dice images
 var randomNumber1 = Math.ceil(Math.random() * 6);
 var randomNumber2 = Math.ceil(Math.random() * 6);
 
@@ -7,3 +8,16 @@ dice2 = dice[1];
 
 dice1.setAttribute('src', 'images/dice' + randomNumber1 + '.png');
 dice2.setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+
+// Judgement
+var statement = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+    statement.textContent = "🚩Player 1 Wins!";
+    statement.style.fontSize = '6em';
+} else if (randomNumber1 < randomNumber2) {
+    statement.textContent = "Player 2 Wins!🚩";
+    statement.style.fontSize = '6em';
+} else {
+    statement.textContent = "Draw!!"
+}
