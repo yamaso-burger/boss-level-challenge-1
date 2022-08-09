26a22c0818621cdaf11c6eb42d4c9182215800db
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-09 16:03:28 +0900

    make a button and enable to judge when button is clicked

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-// Change dice images
-var randomNumber1 = Math.ceil(Math.random() * 6);
-var randomNumber2 = Math.ceil(Math.random() * 6);
+function duel(){
 
-var dice = document.querySelectorAll('img');
-dice1 = dice[0];
-dice2 = dice[1];
-
-dice1.setAttribute('src', 'images/dice' + randomNumber1 + '.png');
-dice2.setAttribute('src', 'images/dice' + randomNumber2 + '.png');
-
-// Judgement
-var statement = document.querySelector("h1");
-
-if (randomNumber1 > randomNumber2) {
-    statement.textContent = "🚩Player 1 Wins!";
-    statement.style.fontSize = '6em';
-} else if (randomNumber1 < randomNumber2) {
-    statement.textContent = "Player 2 Wins!🚩";
-    statement.style.fontSize = '6em';
-} else {
-    statement.textContent = "Draw!!"
+    // Change dice images
+    var randomNumber1 = Math.ceil(Math.random() * 6);
+    var randomNumber2 = Math.ceil(Math.random() * 6);
+    
+    var dice = document.querySelectorAll('img');
+    dice1 = dice[0];
+    dice2 = dice[1];
+    
+    dice1.setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+    dice2.setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+    
+    // Judgement
+    var statement = document.querySelector("h1");
+    
+    if (randomNumber1 > randomNumber2) {
+        statement.textContent = "🚩Player 1 Wins!";
+        statement.style.fontSize = '6em';
+    } else if (randomNumber1 < randomNumber2) {
+        statement.textContent = "Player 2 Wins!🚩";
+        statement.style.fontSize = '6em';
+    } else {
+        statement.textContent = "Draw!!"
+    }
 }
